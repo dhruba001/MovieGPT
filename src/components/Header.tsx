3eba4eb1c4aa405997996e3ce3dd0ba4fd37a7c8
Dhruba <gdhruba748+github@gmail.com>
2025-08-06 15:33:29 +0530

    initial page structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,13 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="absolute top-0 left-0 right-0 px-8 py-2 bg-linear-to-b from-black to-transparent">
+      <img
+        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-07-24/consent/87b6a5c0-0104-4e96-a291-092c11350111/019808e2-d1e7-7c0f-ad43-c485b7d9a221/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="Nteflix_Logo"
+        className="w-25 "
+      />
+    </div>
+  );
 };
 
 export default Header;
