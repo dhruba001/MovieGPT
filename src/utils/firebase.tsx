af6b620cb02bbcec8879567073d651e04dd7b96c
Dhruba <gdhruba748+github@gmail.com>
2025-08-13 16:53:30 +0530

    updated gitignore

--- src/utils/firebase.tsx
+++ src/utils/firebase.tsx
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBtLawSIamaTuq8z4aHgDsh79LSQqlVDgg",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "moviegpt-de581.firebaseapp.com",
   projectId: "moviegpt-de581",
   storageBucket: "moviegpt-de581.firebasestorage.app",
